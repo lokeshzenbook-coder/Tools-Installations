e4becd1e6bbeaedb81d7f1a297199c913d39d4e7
lokeshzenbook-coder <lokeshzenbook@gmail.com>
2026-06-06 13:12:22 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -28,3 +28,8 @@ tar -xzf "eksctl_$PLATFORM.tar.gz" -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+
